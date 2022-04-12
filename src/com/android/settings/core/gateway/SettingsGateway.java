662f68fa5c5bd27d0f38887e63fe8ef755cdbe7b
Anushek Prasal <anushekprasal@gmail.com>
2022-04-12 13:12:46 +0800

    Settings: Introduce automated Sleep Mode [2/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -103,6 +103,7 @@ import com.android.settings.fuelgauge.AdvancedPowerUsageDetail;
 import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverScheduleSettings;
 import com.android.settings.fuelgauge.batterysaver.BatterySaverSettings;
+import com.android.settings.fuelgauge.sleepmode.SleepMode;
 import com.android.settings.gestures.AssistGestureSettings;
 import com.android.settings.gestures.ButtonNavigationSettingsFragment;
 import com.android.settings.gestures.DoubleTapPowerSettings;
@@ -354,7 +355,8 @@ public class SettingsGateway {
             riceDroidSettingsLayout.class.getName(),
             WifiTetherClientManager.class.getName(),
             RunningServices.class.getName(),
-            SmartPixels.class.getName()
+            SmartPixels.class.getName(),
+            SleepMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -400,6 +402,7 @@ public class SettingsGateway {
             Settings.riceDroidSettingsLayoutActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
-            Settings.PreviouslyConnectedDeviceDashboardActivity.class.getName()
+            Settings.PreviouslyConnectedDeviceDashboardActivity.class.getName(),
+            Settings.SleepModeActivity.class.getName()
     };
 }
